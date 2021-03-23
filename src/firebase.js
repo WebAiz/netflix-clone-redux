52f47eb938d9a3e1995ba4c29a5ec1749b7c7158
Aizada <webaiz>
2021-03-23 15:20:03 +0600

    completed and api was hided from public

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,16 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 import firebase from 'firebase'
+import firebaseConfig from './firebaseAPI'
 
-const firebaseConfig = {
-    apiKey: "AIzaSyArJ8HbLLlZoCqRvg2uoe6aw6q77xOtKio",
-    authDomain: "netflix-clone-redux-94cf3.firebaseapp.com",
-    projectId: "netflix-clone-redux-94cf3",
-    storageBucket: "netflix-clone-redux-94cf3.appspot.com",
-    messagingSenderId: "1073674410034",
-    appId: "1:1073674410034:web:0886ada4be3459fa26442b",
-    measurementId: "G-6DKEEFMNDE"
-};
+// const firebaseConfig = {
+//     apiKey: 
+//     authDomain: 
+//     projectId: 
+//     storageBucket: 
+//     messagingSenderId: 
+//     appId: 
+//     measurementId: 
+// };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
---
+++ src/firebaseAPI.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyArJ8HbLLlZoCqRvg2uoe6aw6q77xOtKio",
+    authDomain: "netflix-clone-redux-94cf3.firebaseapp.com",
+    projectId: "netflix-clone-redux-94cf3",
+    storageBucket: "netflix-clone-redux-94cf3.appspot.com",
+    messagingSenderId: "1073674410034",
+    appId: "1:1073674410034:web:0886ada4be3459fa26442b",
+    measurementId: "G-6DKEEFMNDE"
+};
+export default firebaseConfig
